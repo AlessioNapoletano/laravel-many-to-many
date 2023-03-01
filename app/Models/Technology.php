5188f02299a384bc21c2fce203fba721590f7522
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-03-01 16:21:36 +0100

    Create class model 'Technology'

---
+++ app/Models/Technology.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Technology extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'bg_color', 'slug' ];
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    //Relationship method
+    public function projects(){
+        return $this->belongsToMany(projects::class);
+    }
+}
